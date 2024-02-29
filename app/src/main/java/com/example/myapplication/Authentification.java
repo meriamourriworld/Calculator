ba8737c3cd19d4f80cabfd84945dd88b496c3ab0
Meriam Ourri <ourrimeriam@gmail.com>
2024-02-29 15:01:18 +0100

    Added Nouveau compte activity with RETOUR feature

--- app/src/main/java/com/example/myapplication/Authentification.java
+++ app/src/main/java/com/example/myapplication/Authentification.java
@@ -2,13 +2,45 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Authentification extends AppCompatActivity {
 
+    Button btnAuthValider, btnNouveauCompte;
+    EditText edAuthMail, edAuthMp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentification);
+
+        //Récupérer les composants
+        edAuthMail = findViewById(R.id.edAuthMail);
+        edAuthMp = findViewById(R.id.edAuthmp);
+        btnAuthValider = findViewById(R.id.btnAuthValider);
+        btnNouveauCompte = findViewById(R.id.btnNouveauCompte);
+
+        //ÉVÉNEMENTS
+        btnNouveauCompte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(Authentification.this, nouveauCompte.class);
+            }
+        });
+        btnAuthValider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //checking if empty or not a user
+            }
+        });
+    }
+    public static void redirectActivity(Activity activite, Class activite1)
+    {
+        Intent intent = new Intent(activite, activite1);
+        activite.startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/nouveauCompte.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class nouveauCompte extends AppCompatActivity {
+    EditText edNAccNom, edNAccMail, edNAccMp;
+    Button btnNAccCreer, btnNAccRetour;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nouveau_compte);
+
+        //Récupérer les composants
+        edNAccNom = findViewById(R.id.edNewAccountNom);
+        edNAccMail = findViewById(R.id.edNewAccountMail);
+        edNAccMp = findViewById(R.id.edNewAccountMp);
+        btnNAccCreer = findViewById(R.id.btnNAccCreer);
+        btnNAccRetour = findViewById(R.id.btnNewAccountRetour);
+
+        //ÉVÉNEMENTS
+        btnNAccRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectActivity(nouveauCompte.this, Authentification.class);
+                finish();
+            }
+        });
+    }
+    public static void redirectActivity(Activity activite, Class activite1)
+    {
+        Intent intent = new Intent(activite, activite1);
+        activite.startActivity(intent);
+    }
+}
