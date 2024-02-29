4edf27f6abb324167cb5526b429022533cd0ed15
Meriam Ourri <ourrimeriam@gmail.com>
2024-02-29 15:38:08 +0100

    Added Client class

---
+++ app/src/main/java/Client.java
@@ -0,0 +1,47 @@
+public class Client {
+    private int idClient;
+    private String nom;
+    private String email;
+    private String motPasse;
+
+    public Client(String nom, String email, String motPasse)
+    {
+        this.nom = nom;
+        this.email = email;
+        this.motPasse = motPasse;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMotPasse() {
+        return motPasse;
+    }
+
+    public void setMotPasse(String motPasse) {
+        this.motPasse = motPasse;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "idClient=" + idClient +
+                ", nom='" + nom + '\'' +
+                ", email='" + email + '\'' +
+                ", motPasse='" + motPasse + '\'' +
+                '}';
+    }
+}
